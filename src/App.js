f872c9e8ed90bd7d914777d93205ca79e63641f3
Ruslow <you@example.com>
2021-09-14 21:26:13 +0300

    fix little error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SingleFlag from "./pages/SingleFlag";
 import Navbar from "./components/Navbar";
